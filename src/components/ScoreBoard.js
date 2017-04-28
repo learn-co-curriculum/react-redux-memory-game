49ec9b00b908ae84e2624d2a60a27343b4d08212
Luke Ghenco <lukeghenco@gmail.com>
2017-04-28 08:20:24 -0400

    Add basic ScoreBoard component

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ScoreBoard = ({ score }) => 
+  <div className="score__board">
+    <h3>Score: {score}</h3>
+  </div>;
+
+export default ScoreBoard;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Cards from '../components/Cards'
+import ScoreBoard from '../components/ScoreBoard';
+import Cards from '../components/Cards';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="centered">
+        <ScoreBoard score={33}/>
         <Cards />
       </div>
     );
