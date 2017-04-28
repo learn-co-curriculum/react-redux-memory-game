9bc13f9c607c319b6529708b6306cae91e765bf9
Luke Ghenco <lukeghenco@gmail.com>
2017-04-28 08:00:14 -0400

    Add Cards component and update App.js

---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cards extends Component {
+  render() {
+    return (
+      <div>
+        Cards Component
+      </div>
+    )
+  }
+}
+
+export default Cards;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Cards from '../components/Cards'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Cards />
       </div>
     );
   }
