41c9816f15ead14e3bf53096e6aa6ad0d92eb92e
Luke Ghenco <lukeghenco@gmail.com>
2017-04-28 09:00:51 -0400

    Update deck.js code attributes

--- src/data/deck.js
+++ src/data/deck.js
@@ -1,62 +1,62 @@
 export default [
   {
     id: 1,
-    code: "var a = 'a'"
+    code: "let a = 'a'",
     displayed: false
   },
   {
     id: 2, 
-    code: "var a = 'a'"
+    code: "let a = 'a'",
     displayed: false
   },
   {
-    id: 3
-    code: "const goFish = (card) => cards.push(card))",
+    id: 3,
+    code: "goFish()",
     displayed: false
   },
   {
     id: 4,
-    code: "const goFish = (card) => cards.push(card))"
+    code: "goFish()",
     displayed: false
   },
   {
     id: 5,
-    code: "let playTime = currentTime || Date.new()",
+    code: "Date.new()",
     displayed: false
   },
   {
     id: 6,
-    code: "let playTime = currentTime || Date.new()",
+    code: "Date.new()",
     displayed: false
   },
   {
     id: 7,
-    code: "JavaScript > RUBY",
+    code: "JS > RUBY",
     displayed: false
   },
   {
     id: 8,
-    code: "JavaScript > RUBY",
+    code: "JS > RUBY",
     displayed: false
   },
   {
     id: 9,
-    code: "Ruby > JavaScript",
+    code: "Ruby > JS",
     displayed: false
   },
   {
     id: 10,
-    code: "Ruby > JavaScript",
+    code: "Ruby > JS",
     displayed: false
   },
   {
     id: 11,
-    code: "console.log(this)",
+    code: "console.log(a)",
     displayed: false
   },
   {
     id: 12,
-    code: "console.log(this)",
+    code: "console.log(a)",
     displayed: false
   }
 ]
