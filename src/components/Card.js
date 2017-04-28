ec31b511b3cf628873c4d106cfd4086bcbbfce7d
Luke Ghenco <lukeghenco@gmail.com>
2017-04-28 09:01:18 -0400

    Update Card component to show displayed by default

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Card = ({ id, code, displayed }) => 
+  <div className="card">
+    {!displayed &&
+      <h4>{code}</h4>
+    }
+  </div>;
+
+export default Card;
