1b8b88bb4d9049650563f929c58c28d9df6fb39f
Luke Ghenco <lukeghenco@gmail.com>
2017-04-28 08:16:53 -0400

    Build out initial dom rendering of Cards component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 class Cards extends Component {
   render() {
+
+    var cards = [1,2,3,4,5,6,7,8,9,10,11,12].map((card, index) =>
+      <div className="card">
+        <h1>{card}</h1>
+      </div>
+    )
     return (
-      <div>
-        Cards Component
+      <div className="cards">
+        {cards}
       </div>
     )
   }
