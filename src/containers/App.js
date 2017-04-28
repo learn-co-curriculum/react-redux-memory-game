0d189185a0119a03932342afc8f1d57ea54594af
Luke Ghenco <lukeghenco@gmail.com>
2017-04-28 08:33:40 -0400

    Add data for deck

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import ScoreBoard from '../components/ScoreBoard';
 import Cards from '../components/Cards';
 import './App.css';
+import deck from '../data/deck'
 
 class App extends Component {
   render() {
     return (
       <div className="centered">
         <ScoreBoard score={33}/>
-        <Cards />
+        <Cards deck={deck}/>
       </div>
     );
   }
---
+++ src/data/deck.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: 1,
+    code: "var a = 'a'"
+    displayed: false
+  },
+  {
+    id: 2, 
+    code: "var a = 'a'"
+    displayed: false
+  },
+  {
+    id: 3
+    code: "const goFish = (card) => cards.push(card))",
+    displayed: false
+  },
+  {
+    id: 4,
+    code: "const goFish = (card) => cards.push(card))"
+    displayed: false
+  },
+  {
+    id: 5,
+    code: "let hour = currentTime || Date.new()",
+    displayed: false
+  },
+  {
+    id: 6,
+    code: "let hour = currentTime || Date.new()",
+    displayed: false
+  },
+  {
+    id: 7,
+    code: "JavaScript > RUBY",
+    displayed: false
+  },
+  {
+    id: 8,
+    code: "JavaScript > RUBY",
+    displayed: false
+  },
+  {
+    id: 9,
+    code: "Ruby > JavaScript",
+    displayed: false
+  },
+  {
+    id: 10,
+    code: "Ruby > JavaScript",
+    displayed: false
+  },
+  {
+    id: 11,
+    code: "console.log(this)",
+    displayed: false
+  },
+  {
+    id: 12,
+    code: "console.log(this)",
+    displayed: false
+  }
+]
